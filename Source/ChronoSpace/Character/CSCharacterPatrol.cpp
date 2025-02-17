f0f0d99c48648204967e5e3081559b36884b24dc
Lagooneng <hyunwoo122490@gmail.com>
2025-02-18 04:57:11 +0900

    Patrol Mesh

--- Source/ChronoSpace/Character/CSCharacterPatrol.cpp
+++ Source/ChronoSpace/Character/CSCharacterPatrol.cpp
@@ -19,6 +19,12 @@ ACSCharacterPatrol::ACSCharacterPatrol()
 {
 	GetMesh()->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
 
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> CharacterMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Chibi_characters/Meshes/SK_Chibi_Skullboy.SK_Chibi_Skullboy'"));
+	if (CharacterMeshRef.Object)
+	{
+		GetMesh()->SetSkeletalMesh(CharacterMeshRef.Object);
+	}
+
 	AIControllerClass = ACSAIController::StaticClass();
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 	GetCharacterMovement()->GetNavMovementProperties()->bUseAccelerationForPaths = true;
